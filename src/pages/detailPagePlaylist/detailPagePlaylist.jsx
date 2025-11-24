8f242af3356a586b261a58769d210115d4bfd186
Léo Maurice <l.maurice8361@gmail.com>
2025-11-24 17:52:39 +0100

    feat: ✨ enhance PlaylistPage with token requirement and URL ID display

--- src/pages/detailPagePlaylist/detailPagePlaylist.jsx
+++ src/pages/detailPagePlaylist/detailPagePlaylist.jsx
@@ -1,3 +1,21 @@
+import { useState, useEffect } from 'react';
+import { buildTitle } from '../../constants/appMeta.js';
+import { useRequireToken } from '../../hooks/useRequireToken.js';
+import { fetchPlaylistById } from '../../api/spotify-playlists.js';
+import { handleTokenError } from '../../utils/handleTokenError.js';
+import '../PageLayout.css';
+import { useNavigate, useParams } from 'react-router-dom';
+
+
 export default function PlaylistPage() {
-    return <div>Playlist Page</div>;
+    const { id } = useParams();
+    // require token to fetch playlists
+    const { token } = useRequireToken();
+
+    return (
+        <div>
+            Playlist Page
+            <div>ID dans l'URL: {id ?? 'aucun id'}</div>
+        </div>
+    );
 }
